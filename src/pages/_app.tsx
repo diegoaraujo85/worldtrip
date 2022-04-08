60713946e537562a259c91144528ef6fb9a946b9
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-04-08 07:45:01 -0300

    final project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
+import "swiper/swiper-bundle.css";
+import "../styles/swiper.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
